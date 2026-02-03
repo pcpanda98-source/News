807ee9cc5d37f6a9bfa5777adb899729df24d9c3
pcpanda98-source <p.c.panda98@gmail.com>
2026-02-03 18:13:15 +0000

    ...

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from app import create_app
 import os
 
+# Create app instance for Gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     cert = os.getenv('SSL_CERT')
     key = os.getenv('SSL_KEY')
     if cert and key:
