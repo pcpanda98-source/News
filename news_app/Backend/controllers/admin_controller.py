eeee3bccfaca19019d970fca87fa431f1112142c
Praveen <p.c.panda98@gmail.com>
2026-02-11 09:05:41 +0000

    Add /manage and /bookmarks routes

--- news_app/Backend/controllers/admin_controller.py
+++ news_app/Backend/controllers/admin_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from ..services.article_service import list_articles
 from ..services.category_service import list_categories
+from ..services.media_service import list_media
 
 admin_bp = Blueprint('admin', __name__, template_folder='templates')
 
@@ -10,4 +11,19 @@ def dashboard():
     articles = list_articles()
     categories = list_categories()
     return render_template('admin_dashboard.html', articles=articles, categories=categories)
+
+
+@admin_bp.route('/manage')
+def manage_articles():
+    """Manage articles page"""
+    articles = list_articles()
+    categories = list_categories()
+    return render_template('manage_articles.html', articles=articles, categories=categories)
+
+
+@admin_bp.route('/bookmarks')
+def bookmarks():
+    """Bookmarks page"""
+    articles = list_articles()
+    return render_template('bookmarks.html', articles=articles)
 
